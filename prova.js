f379243fca3f726fc1faa39b266eefcc84f316cc
ImKuros <manganaoegib0077@gmail.com>
2026-07-10 23:36:11 +0000

    mutiplicação

--- prova.js
+++ prova.js
@@ -5,3 +5,11 @@ function adicao (v1,v2){
 function subtracao (v1,v2){
     return v1-v2
 }
+function mutiplicacao (v1,v2){
+    return v1*v2
+}
+
+//function subtracao (v1,v2){
+  //  return v1/v2
+//}
+
